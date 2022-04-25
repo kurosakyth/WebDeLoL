00e4c3962aa1e456a2dd712f94827d6d055dcfb9
aasan <aasantamariac@hotmail.es>
2022-04-24 18:07:52 -0600

    conexión a base de datos de imagen de fondo

--- sistema/lolcito/models.py
+++ sistema/lolcito/models.py
@@ -1,3 +1,9 @@
+from ast import Delete
 from django.db import models
 
 # Create your models here.
+#captura la estructura la tabla libros en este caso
+
+class Fondo(models.Model):
+    id = models.AutoField(primary_key=True)
+    imagen = models.ImageField(upload_to = 'images/', verbose_name='Imagen',null = True)
