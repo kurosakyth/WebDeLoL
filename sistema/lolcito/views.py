9663163e52c5ef38c8ad11c1de85cc8c8bdae931
aasan <aasantamariac@hotmail.es>
2022-04-25 23:55:56 -0600

    intentando agregar imagen de fondo

--- sistema/lolcito/views.py
+++ sistema/lolcito/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from lolcito.models import Fondo
+
 # Create your views here.
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
+def fondo(request):
+    fondo = Fondo.objects.all()
+    return render(request, 'paginas/inicio.html', {'fondo': fondo})
+
